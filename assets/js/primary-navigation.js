96e7e2f2aa3cdf0c06bf1113ee7984cc246d7cbb
js-wp-admin <js-wp-admin@dev.kevinnlsamuel.com>
2021-12-27 14:50:17 +0000

    import twentytwentyone

---
+++ assets/js/primary-navigation.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:896a30041e1249377848e9351f2fa7b064c64bcb75cb1bbcac7230284d9f1a9f
+size 6018
---
+++ postcss.config.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:985152fd7d80dbdbe7b4afddd5372cfd767335478a49c17dcb6973d591e2c883
+size 249
